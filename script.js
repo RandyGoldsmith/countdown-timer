b594ea047ab09161ccb93bd7885d85891e795b6f
Randy <rgoldshz@gmail.com>
2021-04-22 11:16:35 -0500

    get user form input and set min date

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
+const title = document.getElementById('title');
+const inputDate = document.getElementById('date-picker');
+const formData = document.getElementById('form');
 
+
+let titleValue = '';
+let dateValue = '';
+const today = new Date().toISOString().split("T")[0];
+
+// set min date so user can't pick any dates in past
+inputDate.setAttribute('min', today);
+
+
+function getData(e) {
+    e.preventDefault();
+    titleValue = e.srcElement[0].value;
+    dateValue = e.srcElement[1].value;
+    console.log(titleValue, dateValue);
+}
+
+formData.addEventListener('submit', getData);
 
