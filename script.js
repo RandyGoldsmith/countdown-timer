cce193fc7cfedd70005cd57242bce7907bd89edb
Randy <rgoldshz@gmail.com>
2021-04-26 11:51:15 -0500

    completed reset buttons functionality

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ const countdownTitle = document.getElementById('countdown-title');
 const countdownTimer = document.getElementById('countdown-timer');
 const time = document.querySelectorAll('span');
 const inputContainer = document.getElementById('input-container');
+const resetBtn = document.getElementById('reset');
+const completeBtn = document.getElementById('reset2');
+const completeContainer = document.getElementById('countdown-complete');
 
 
 let countTitle = '';
 let countDate = '';
 let countDateValue = new Date();
+let activeCount;
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
@@ -20,36 +24,49 @@ const today = new Date().toISOString().split("T")[0];
 inputDate.setAttribute('min', today);
 
 function countdown() {
-    const todayValue = new Date().getTime();
-    console.log(todayValue);
-    const difference = countDateValue - todayValue;
-    console.log('distance', difference);
-    const days = Math.floor(difference / day);
-    const hours = Math.floor((difference % day) / hour);
-    const minutes = Math.floor((difference % hour) / minute);
-    const seconds = Math.floor((difference % minute) / second);
-    console.log(days, hours, minutes, seconds);
-    // show data
-    countdownTitle.textContent = `${countTitle}`;
-    time[0].textContent = `${days}`;
-    time[1].textContent = `${hours}`;
-    time[2].textContent = `${minutes}`;
-    time[3].textContent = `${seconds}`;
-
-    inputContainer.hidden = true;
-    countdownTimer.hidden = false;
+    activeCount = setInterval(()=> {
+        const todayValue = new Date().getTime();
+        const difference = countDateValue - todayValue;
+        const days = Math.floor(difference / day);
+        const hours = Math.floor((difference % day) / hour);
+        const minutes = Math.floor((difference % hour) / minute);
+        const seconds = Math.floor((difference % minute) / second);
+    
+        if(difference < 0) {
+            countdownTimer.hidden = true;
+            completeContainer.hidden = false;
+        } else {
+            countdownTitle.textContent = `${countTitle}`;
+            time[0].textContent = `${days}`;
+            time[1].textContent = `${hours}`;
+            time[2].textContent = `${minutes}`;
+            time[3].textContent = `${seconds}`;
+        }
+        
+        inputContainer.hidden = true;
+        
+    }, second);
 }
 
 
 function getData(e) {
     e.preventDefault();
     countTitle = e.srcElement[0].value;
-    countValue = e.srcElement[1].value;
-    console.log(countTitle, countValue);
+    countValue = e.srcElement[1].value; 
     countDateValue = new Date(countValue).getTime();
-    console.log('countdown value:', countDateValue);
     countdown();
 }
 
+function reset() {
+    clearInterval(activeCount);
+    countTitle = '';
+    countDate = '';
+    inputContainer.hidden = false;
+    countdownTimer.hidden = true;
+    completeContainer.hidden = true;
+}
+
 formData.addEventListener('submit', getData);
+resetBtn.addEventListener('click', reset);
+completeBtn.addEventListener('click', reset);
 
